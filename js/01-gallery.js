4083bfbedc0974cbf7163325a41c4d658f57336e
Musiyan98 <musiyan98@gmail.com>
2022-09-26 23:46:11 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector(".gallery");
+
+createGalleryGrid(galleryItems);
+
+galleryBox.addEventListener("click", onOpenModal);
+
+function onOpenModal(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+		<img src="${e.target.dataset.source}">
+	`);
+  instance.show();
+}
+
+function createGalleryGrid(obj) {
+  const galleryGrid = obj.map(createGalleryItem);
+  galleryBox.insertAdjacentHTML("beforeend", galleryGrid.join(" "));
+  //   console.log(galleryGrid);
+}
+
+function createGalleryItem({ preview, original, description }) {
+  return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+}
